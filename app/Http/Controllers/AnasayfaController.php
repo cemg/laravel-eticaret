2f4eef9a692d81e486bc6105ccfb7253acab4360
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-09-24 19:31:52 +0300

    2.4 View Yapısı ve Blade Template Engine kodları kaldırıldı.

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -8,18 +8,6 @@ class AnasayfaController extends Controller
 {
     public function index()
     {
-        $isim = "Cem";
-        $soyisim = "Gündüzoğlu";
-        $isimler = ['Cem', 'Esra', 'Murat', 'Ebru', 'Aydın'];
-        $kullanicilar = [
-            ['id'=>1, 'kullanici_adi'=> 'Cem'],
-            ['id'=>2, 'kullanici_adi'=> 'Esra'],
-            ['id'=>3, 'kullanici_adi'=> 'Murat'],
-            ['id'=>4, 'kullanici_adi'=> 'Ebru'],
-            ['id'=>5, 'kullanici_adi'=> 'Aydın']
-        ];
-        //return view('anasayfa', ['isim'=> 'Cem', 'soyisim'=> 'Gündüzoğlu']);
-        return view('anasayfa', compact('isim', 'soyisim', 'isimler', 'kullanicilar'));
-        //return view('anasayfa')->with(['isim'=> $isim, 'soyisim'=> $soyisim]);
+        return view('anasayfa');
     }
 }
--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -7,61 +7,6 @@
     <title>Eticaret Projesi</title>
 </head>
 <body>
-@php
-$yas = 29;
-@endphp
-Merhaba {{ $isim . ' ' . $soyisim }}, {{ $yas }} yaşındasın.
-<hr>
-@if ($isim == 'Cem')
-    Hoşgeldin Cem
-@elseif ($isim == 'Esra')
-    Hoşgeldin Esra
-@else
-    Hoşgeldin
-@endif
-<hr>
-@switch($isim)
-    @case('Cem')
-    Hoşgeldin Cem
-    @break
-
-    @case('Esra')
-    Hoşgeldin Esra
-    @break
-
-    @default
-    Hoşgeldin
-@endswitch
-<hr>
-@for($i=0; $i<=10; $i++)
-    Döngü Değeri: {{ $i }}
-@endfor
-<hr>
-@php
-$i=0;
-@endphp
-@while($i<=10)
-    Döngü Değeri: {{ $i }}
-    @php
-    $i++;
-    @endphp
-@endwhile
-<hr>
-@foreach($isimler as $isim)
-    {{ $isim . ($isim !== end($isimler) ? ', ' : '') }}
-@endforeach
-<hr>
-@foreach($kullanicilar as $kullanici)
-    @continue($kullanici['id'] == 1)
-    <li>{{ $kullanici['id'] . '-' . $kullanici['kullanici_adi'] }}</li>
-    @break($kullanici['id'] == 4)
-@endforeach
-{{-- Yorum satırları --}}
-<hr>
-@php
-$html = "<b>Test</b>";
-@endphp
-{!! $html !!}
-
+Merhaba
 </body>
 </html>
