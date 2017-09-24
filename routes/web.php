3c3468d8179c2ff466b72e5536bb7900b644dd91
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-09-24 14:06:07 +0300

    2.2. Route Yapısı

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/merhaba', function() {
+    return "Merhaba";
+});
+
+Route::get('/api/v1/merhaba', function() {
+   return ['mesaj'=>'Merhaba'];
+});
+
+Route::get('/urun/{isim}/{id?}', function($urunadi, $id=0) {
+    return "Ürün Adı: $id $urunadi";
+})->name('urun_detay');
+
+Route::get('/kampanya', function() {
+   return redirect()->route('urun_detay', ['id'=>5, 'isim'=>'elma']);
+});
