2a7da71fc236aa904fa7aa69aefc5a763c8e6f9c
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-09-24 19:30:29 +0300

    2.4 View Yapısı ve Blade Template Engine

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -8,6 +8,18 @@ class AnasayfaController extends Controller
 {
     public function index()
     {
-        return view('anasayfa');
+        $isim = "Cem";
+        $soyisim = "Gündüzoğlu";
+        $isimler = ['Cem', 'Esra', 'Murat', 'Ebru', 'Aydın'];
+        $kullanicilar = [
+            ['id'=>1, 'kullanici_adi'=> 'Cem'],
+            ['id'=>2, 'kullanici_adi'=> 'Esra'],
+            ['id'=>3, 'kullanici_adi'=> 'Murat'],
+            ['id'=>4, 'kullanici_adi'=> 'Ebru'],
+            ['id'=>5, 'kullanici_adi'=> 'Aydın']
+        ];
+        //return view('anasayfa', ['isim'=> 'Cem', 'soyisim'=> 'Gündüzoğlu']);
+        return view('anasayfa', compact('isim', 'soyisim', 'isimler', 'kullanicilar'));
+        //return view('anasayfa')->with(['isim'=> $isim, 'soyisim'=> $soyisim]);
     }
 }
--- resources/views/anasayfa.blade.php
+++ resources/views/anasayfa.blade.php
@@ -1,12 +1,67 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Eticaret Projesi</title>
-    </head>
-    <body>
-        Merhaba
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eticaret Projesi</title>
+</head>
+<body>
+@php
+$yas = 29;
+@endphp
+Merhaba {{ $isim . ' ' . $soyisim }}, {{ $yas }} yaşındasın.
+<hr>
+@if ($isim == 'Cem')
+    Hoşgeldin Cem
+@elseif ($isim == 'Esra')
+    Hoşgeldin Esra
+@else
+    Hoşgeldin
+@endif
+<hr>
+@switch($isim)
+    @case('Cem')
+    Hoşgeldin Cem
+    @break
+
+    @case('Esra')
+    Hoşgeldin Esra
+    @break
+
+    @default
+    Hoşgeldin
+@endswitch
+<hr>
+@for($i=0; $i<=10; $i++)
+    Döngü Değeri: {{ $i }}
+@endfor
+<hr>
+@php
+$i=0;
+@endphp
+@while($i<=10)
+    Döngü Değeri: {{ $i }}
+    @php
+    $i++;
+    @endphp
+@endwhile
+<hr>
+@foreach($isimler as $isim)
+    {{ $isim . ($isim !== end($isimler) ? ', ' : '') }}
+@endforeach
+<hr>
+@foreach($kullanicilar as $kullanici)
+    @continue($kullanici['id'] == 1)
+    <li>{{ $kullanici['id'] . '-' . $kullanici['kullanici_adi'] }}</li>
+    @break($kullanici['id'] == 4)
+@endforeach
+{{-- Yorum satırları --}}
+<hr>
+@php
+$html = "<b>Test</b>";
+@endphp
+{!! $html !!}
+
+</body>
 </html>
