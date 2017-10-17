270306fcf978f1e1d1d1bfc5715e74de47cd18a6
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-10-17 22:10:52 +0300

    4.1. Kategori Migration Dosyası oluşturuldu.

---
+++ database/migrations/2017_10_17_183408_create_kategori_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKategoriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategori', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kategori_adi', 30);
+            $table->string('slug', 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategori');
+    }
+}
