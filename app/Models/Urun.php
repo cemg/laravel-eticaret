cc14ce2a5eb364e2277b84f4674e45db792cb106
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-12 00:16:18 +0300

    5.2. Ürün Model Dosyası

---
+++ app/Models/Urun.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Urun extends Model
+{
+    use SoftDeletes;
+    
+    protected $table = "urun";
+    
+    protected $guarded = [];
+    
+    const CREATED_AT = 'olusturma_tarihi';
+    const UPDATED_AT = 'guncelleme_tarihi';
+    const DELETED_AT = 'silinme_tarihi';
+}
