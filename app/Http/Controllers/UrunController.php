3da833fd32c550f3866c3b22af96705f0a94be0a
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-12 00:33:50 +0300

    5.6. Ürün Sayfasının Gösterilmesi

--- app/Http/Controllers/UrunController.php
+++ app/Http/Controllers/UrunController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Urun;
 
 class UrunController extends Controller
 {
     public function index($slug_urunadi)
     {
-        return view('urun');
+        $urun = Urun::whereSlug($slug_urunadi)->firstOrFail();
+        $kategoriler = $urun->kategoriler()->distinct()->get();
+        return view('urun', compact('urun', 'kategoriler'));
     }
 }
--- resources/views/urun.blade.php
+++ resources/views/urun.blade.php
@@ -1,32 +1,34 @@
 @extends('layouts.master')
-@section('title', 'Ürün')
+@section('title', $urun->urun_adi)
 @section('content')
     <div class="container">
         <ol class="breadcrumb">
             <li><a href="#">Anasayfa</a></li>
-            <li><a href="#">Kategori</a></li>
-            <li class="active">Kategori</li>
+            @foreach($kategoriler as $kategori)
+            <li><a href="{{ route('kategori', $kategori->slug) }}">{{ $kategori->kategori_adi }}</a></li>
+            @endforeach
+            <li class="active">{{ $urun->urun_adi }}</li>
         </ol>
         <div class="bg-content">
             <div class="row">
                 <div class="col-md-5">
-                    <img src="http://lorempixel.com/400/200/food/1">
+                    <img src="http://via.placeholder.com/400x200?text=UrunResmi">
                     <hr>
                     <div class="row">
                         <div class="col-xs-3">
-                            <a href="#" class="thumbnail"><img src="http://lorempixel.com/60/60/food/2"></a>
+                            <a href="#" class="thumbnail"><img src="http://via.placeholder.com/60x60?text=UrunResmi"></a>
                         </div>
                         <div class="col-xs-3">
-                            <a href="#" class="thumbnail"><img src="http://lorempixel.com/60/60/food/3"></a>
+                            <a href="#" class="thumbnail"><img src="http://via.placeholder.com/60x60?text=UrunResmi"></a>
                         </div>
                         <div class="col-xs-3">
-                            <a href="#" class="thumbnail"><img src="http://lorempixel.com/60/60/food/4"></a>
+                            <a href="#" class="thumbnail"><img src="http://via.placeholder.com/60x60?text=UrunResmi"></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-7">
-                    <h1>Ürün adı</h1>
-                    <p class="price">129 ₺</p>
+                    <h1>{{ $urun->urun_adi }}</h1>
+                    <p class="price">{{ $urun->fiyati }} ₺</p>
                     <p><a href="#" class="btn btn-theme">Sepete Ekle</a></p>
                 </div>
             </div>
@@ -37,8 +39,12 @@
                     <li role="presentation"><a href="#t2" data-toggle="tab">Yorumlar</a></li>
                 </ul>
                 <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane active" id="t1">t1</div>
-                    <div role="tabpanel" class="tab-pane" id="t2">t2</div>
+                    <div role="tabpanel" class="tab-pane active" id="t1">
+                        {{ $urun->aciklama }}
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="t2">
+                        Henüz yorum yapılmadı!
+                    </div>
                 </div>
             </div>
 
