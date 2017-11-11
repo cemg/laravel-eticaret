63941edc2f7b6402f0699006a95f3f1976d06259
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-12 01:16:28 +0300

    6.1. Kullanıcı Migration Dosyası

---
+++ database/migrations/2017_11_10_191439_create_kullanici_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKullaniciTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kullanici', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('adsoyad', 60);
+            $table->string('email', 150)->unique();
+            $table->string('sifre', 60);
+            $table->string('aktivasyon_anahtari', 60)->nullable();
+            $table->boolean('aktif_mi')->default(0);
+    
+            $table->timestamp('olusturma_tarihi')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('guncelleme_tarihi')->default(DB::raw('CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('silinme_tarihi')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kullanici');
+    }
+}
