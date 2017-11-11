039fcca6d5dd87b8dedca1c3cd1f691219295360
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-12 00:13:47 +0300

    5.1. Ürün Migration Dosyası

---
+++ database/migrations/2017_10_29_114953_create_urun_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUrunTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urun', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug', 160);
+            $table->string('urun_adi', 150);
+            $table->text('aciklama');
+            $table->decimal('fiyati', 6, 3);
+            $table->timestamp('olusturma_tarihi')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('guncelleme_tarihi')->default(DB::raw('CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('silinme_tarihi')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urun');
+    }
+}
