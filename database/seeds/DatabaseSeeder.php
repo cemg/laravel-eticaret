04d894f6307943ff99a16431d74ed1bb078a6cc3
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-12 00:17:27 +0300

    5.3. Ürün Seeder Dosyası ve Faker Kütüphanesi

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UrunTableSeeder::class);
         $this->call(KategoriTableSeeder::class);
+        $this->call(UrunTableSeeder::class);
     }
 }
--- database/seeds/UrunTableSeeder.php
+++ database/seeds/UrunTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Urun;
+use App\Models\UrunDetay;
 use Illuminate\Database\Seeder;
 
 class UrunTableSeeder extends Seeder
@@ -9,11 +11,29 @@ class UrunTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker\Generator $faker)
     {
-        DB::table("urun")->insert([
-            'urun_adi' => str_random(10),
-            'fiyati'   => rand(10, 99) / 10
-        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Urun::truncate();
+        UrunDetay::truncate();
+        
+        for ($i = 0; $i < 30; $i++) {
+            $urun_adi = $faker->streetName;
+            $urun = Urun::create([
+                'urun_adi' => $urun_adi,
+                'slug'     => str_slug($urun_adi),
+                'aciklama' => $faker->paragraph(20),
+                'fiyati'   => $faker->randomFloat(3, 1, 20)
+            ]);
+            
+            $detay = $urun->detay()->create([
+                'goster_slider'=>rand(0,1),
+                'goster_gunun_firsati'=>rand(0,1),
+                'goster_one_cikan'=>rand(0,1),
+                'goster_cok_satan'=>rand(0,1),
+                'goster_indirimli'=>rand(0,1)
+            ]);
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
