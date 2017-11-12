165bc287f3b90abacd4663d89616f8de78774cbd
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-12 23:24:02 +0300

    app.js içerisinde bootstrap kodları otomatik olarak gelediği için master şablon dosyası içerisinde yer alan harici bootsrap javascript dosya bağlantısı kaldırıldı.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,6 @@
 @yield('content')
 @include('layouts.partials.footer')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="/js/app.js"></script>
 @yield('footer')
 </body>
