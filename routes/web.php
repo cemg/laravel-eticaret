723f9e1f5934a24e459b248940c89d56a5e40f51
Cem Gündüzoğlu <cmg.web@gmail.com>
2017-11-13 00:42:07 +0300

    6.9. Auth Middleware Kullanımı

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ Route::get('/ara', 'UrunController@ara')->name('urun_ara');
 
 Route::get('/sepet', 'SepetController@index')->name('sepet');
 
-Route::get('/odeme', 'OdemeController@index')->name('odeme');
-
-Route::get('/siparisler', 'SiparisController@index')->name('siparisler');
-
-Route::get('/siparisler/{id}', 'SiparisController@detay')->name('siparis');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/odeme', 'OdemeController@index')->name('odeme');
+    Route::get('/siparisler', 'SiparisController@index')->name('siparisler');
+    Route::get('/siparisler/{id}', 'SiparisController@detay')->name('siparis');
+});
 
 Route::group(['prefix' => 'kullanici'], function () {
     Route::get('/oturumac', 'KullaniciController@giris_form')->name('kullanici.oturumac');
