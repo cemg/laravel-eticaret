f38c2133f0f32c7fcd31246a63d4661ea87c312c
Cem Gündüzoğlu <cmg.web@gmail.com>
2018-03-06 20:40:27 +0300

    14. Cache Yapısı ve Raporlama, raporların chart.js ile gösterilmesi

--- app/Http/Controllers/Yonetim/AnasayfaController.php
+++ app/Http/Controllers/Yonetim/AnasayfaController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\Yonetim;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class AnasayfaController extends Controller
 {
     public function index()
     {
-        return view('yonetim.anasayfa');
+        $cok_satan_urunler = DB::select("
+            SELECT u.urun_adi, SUM(su.adet) adet
+            FROM siparis si
+            INNER JOIN sepet s ON s.id = si.sepet_id
+            INNER JOIN sepet_urun su ON s.id = su.sepet_id
+            INNER JOIN urun u ON u.id = su.urun_id
+            GROUP BY u.urun_adi
+            ORDER BY SUM(su.adet) DESC
+        ");
+        
+        $aylara_gore_satislar = DB::select("
+            SELECT
+              DATE_FORMAT(si.olusturma_tarihi, '%Y-%m') ay, sum(su.adet) adet
+            FROM siparis si
+            INNER JOIN sepet s on s.id = si.sepet_id
+            INNER JOIN sepet_urun su on s.id=su.sepet_id
+            GROUP BY DATE_FORMAT(si.olusturma_tarihi, '%Y-%m')
+            ORDER BY DATE_FORMAT(si.olusturma_tarihi, '%Y-%m')
+        ");
+    
+        return view('yonetim.anasayfa', compact('cok_satan_urunler', 'aylara_gore_satislar'));
     }
 }
--- resources/views/yonetim/anasayfa.blade.php
+++ resources/views/yonetim/anasayfa.blade.php
@@ -42,5 +42,99 @@
         </div>
     </section>
 
+    <section class="row">
+        <div class="col-sm-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Çok Satan Ürünler</div>
+                <div class="panel-body">
+                    <canvas id="chartCokSatan"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Aylara Göre Satışlar</div>
+                <div class="panel-body">
+                    <canvas id="chartAylaraGoreSatislar"></canvas>
+                </div>
+            </div>
+        </div>
+    </section>
 
+@endsection
+
+@section('footer')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+    <script>
+        @php
+        $labels = "";
+        $data = "";
+        foreach($cok_satan_urunler as $rapor) {
+            $labels .= "\"$rapor->urun_adi\", ";
+            $data .= "$rapor->adet, ";
+        }
+        @endphp
+        var ctx1 = document.getElementById("chartCokSatan").getContext('2d');
+        var chartCokSatan = new Chart(ctx1, {
+            type: 'horizontalBar',
+            data: {
+                labels: [{!! $labels !!}],
+                datasets: [{
+                    label: 'Çok Satan Ürünler',
+                    data: [{!! $data !!}],
+                    borderColor: '#f4645f',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    position: 'bottom',
+                    display: false
+                },
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
+        });
+
+        @php
+        $labels = "";
+        $data = "";
+        foreach($aylara_gore_satislar as $rapor) {
+            $labels .= "\"$rapor->ay\", ";
+            $data .= "$rapor->adet, ";
+        }
+        @endphp
+        var ctx2 = document.getElementById("chartAylaraGoreSatislar").getContext('2d');
+        var chartAylaraGoreSatislar = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                labels: [{!! $labels !!}],
+                datasets: [{
+                    label: 'Aylara Göre Satışlar',
+                    data: [{!! $data !!}],
+                    borderColor: '#f4645f',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    position: 'bottom'
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
 @endsection
