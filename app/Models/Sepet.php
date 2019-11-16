9715ce32744b0117763cbef6f081625a7a71c405
Cem Gündüzoğlu <cmg.web@gmail.com>
2019-11-16 23:38:00 +0300

    sepet_urun_adet metodunda düzenleme

--- app/Models/Sepet.php
+++ app/Models/Sepet.php
@@ -9,25 +9,25 @@ use Illuminate\Support\Facades\DB;
 class Sepet extends Model
 {
     use SoftDeletes;
-    
+
     protected $table = "sepet";
-    
+
     protected $guarded = [];
-    
+
     const CREATED_AT = 'olusturma_tarihi';
     const UPDATED_AT = 'guncelleme_tarihi';
     const DELETED_AT = 'silinme_tarihi';
-    
+
     public function siparis()
     {
         return $this->hasOne('App\Models\Siparis');
     }
-    
+
     public function sepet_urunler()
     {
         return $this->hasMany('App\Models\SepetUrun');
     }
-    
+
     public static function aktif_sepet_id()
     {
         $aktif_sepet = DB::table('sepet as s')
@@ -37,15 +37,18 @@ class Sepet extends Model
             ->orderByDesc('s.olusturma_tarihi')
             ->select('s.id')
             ->first();
-        
+
         if (!is_null($aktif_sepet)) return $aktif_sepet->id;
     }
-    
+
     public function sepet_urun_adet()
     {
-        return DB::table('sepet_urun')->where('sepet_id', $this->id)->sum('adet');
+        return DB::table('sepet_urun')
+            ->whereRaw('silinme_tarihi is null')
+            ->where('sepet_id', $this->id)
+            ->sum('adet');
     }
-    
+
     public function kullanici()
     {
         return $this->belongsTo('App\Models\Kullanici');
